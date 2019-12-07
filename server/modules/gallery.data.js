d02017e416839fc550bc87d2a1c68fa792364678
Allen Lucke <allenlucke@gmail.com>
2019-12-06 19:54:05 -0600

    images rendering on the DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,12 @@
+const express = require('express');
+
+
 const galleryItems = [
-    { id: 1, title: 'Goat Pic', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
-    { id: 2, title: 'Julia in Blanket', path: 'images/img1.jpg', description: 'Photo my wife took of my daughter at about 7 weeks old.', likes: 0 }
-    { id: 3, title: 'Together at last.', path: 'images/img2.jpg', description: 'Photo of us right before we left the hospital.', likes: 0 }
-    { id: 4, title: 'Hello World', path: 'images/img3.jpg', description: 'Photo I took of Julia at about 5 weeks.', likes: 0 }
+    { id: 1, title: 'Goat Pic', path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, title: 'Julia in Blanket', path: 'images/img1.jpg', description: 'Photo my wife took of my daughter at about 7 weeks old.', likes: 0 },
+    { id: 3, title: 'Together at last.', path: 'images/img2.jpg', description: 'Photo of us right before we left the hospital.', likes: 0 },
+    { id: 4, title: 'Hello World', path: 'images/img3.jpg', description: 'Photo I took of Julia at about 5 weeks.', likes: 0 },
+    { id: 5, title: 'a test', path: 'https://images.unsplash.com/photo-1575629521977-9c273642c0a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', description: '', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    galleryList: [],
+    enteredData: {
+      id: '',
+      title: '',
+      path: '',
+      description: '',
+      likes: '',
+    }
+  }
+
+  componentDidMount() {
+    this.getGalleryList();
+  }
+  getGalleryList = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((response) => {
+      this.setState({
+        galleryList: response.data
+      }, () => {
+        console.log(this.state);
+      });
+    })
+    .catch((err) => {
+      console.warn(err);
+    })
+  }
   render() {
+    const galleryListData = this.state.galleryList.map((item, index) => {
+      return (
+      <img src={item.path} key={index} alt="not again"/>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
@@ -10,7 +47,9 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <div>{galleryListData}</div>
+        <img src="images/goat_small.jpg" alt="not again"/>
+        <img src="images/img1.jpg" alt="not again"/>
       </div>
     );
   }
