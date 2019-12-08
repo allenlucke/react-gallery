93044e92c500c5c70bf025c77071afd12bac7cb9
Allen Lucke <allenlucke@gmail.com>
2019-12-07 22:19:56 -0600

    GET route to database functioning proplerly, old routes deleted

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js')
-const galleryItems = require('../modules/gallery.data');
 
-// DO NOT MODIFY THIS FILE FOR BASE MODE
-
-// PUT Route
+//New PUT Route
 router.put('/likes/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+    const queryString = 'UPDATE "react_gallery" SET "likes" = "likes" +1 WHERE "id"=$1;'; 
+    pool.query(queryString, [galleryId])
+    .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+})
+
 //New GET Route
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     const queryString = 'SELECT * FROM "react_gallery" ORDER BY "id" ASC;';
     pool.query(queryString)
     .then((response) => {
@@ -27,9 +28,5 @@ router.get('/', (req,res) => {
         res.sendStatus(500);
     })
 })
-// GET Route
-// router.get('/', (req, res) => {
-//     res.send(galleryItems);
-// }); // END GET Route
 
 module.exports = router;
