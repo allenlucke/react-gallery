79fd74787a049da7f26e7fb48f86e890619a2168
Allen Lucke <allenlucke@gmail.com>
2019-12-07 23:23:46 -0600

    Likes changed to default server in DB, POST route functioning properly on server

--- database.sql
+++ database.sql
@@ -3,10 +3,10 @@ id SERIAL PRIMARY KEY,
 title VARCHAR(80),
 path VARCHAR(100),
 description VARCHAR(250),
-likes INTEGER   
+likes INTEGER DEFAULT 0  
 );
 
-INSERT INTO "react_gallery" ("title", "path", "description", "likes")
-VALUES('Julia in Blanket', 'images/img1.jpg', 'Photo my wife took of my daughter at about 7 weeks old.', 0),
-('Together at last.', 'images/img2.jpg', 'Photo of us right before we left the hospital.', 0),
-('Hello World', 'images/img3.jpg', 'Photo I took of Julia at about 5 weeks.', 0);
+INSERT INTO "react_gallery" ("title", "path", "description")
+VALUES('Julia in Blanket', 'images/img1.jpg', 'Photo my wife took of my daughter at about 7 weeks old.'),
+('Together at last.', 'images/img2.jpg', 'Photo of us right before we left the hospital.'),
+('Hello World', 'images/img3.jpg', 'Photo I took of Julia at about 5 weeks.');
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -6,7 +6,7 @@ const pool = require('../modules/pool.js')
 router.put('/likes/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    const queryString = 'UPDATE "react_gallery" SET "likes" = "likes" +1 WHERE "id"=$1;'; 
+    const queryString = `UPDATE "react_gallery" SET "likes" = "likes" +1 WHERE "id"=$1;`; 
     pool.query(queryString, [galleryId])
     .then((result) => {
         res.sendStatus(200);
@@ -19,7 +19,7 @@ router.put('/likes/:id', (req, res) => {
 
 //New GET Route
 router.get('/', (req, res) => {
-    const queryString = 'SELECT * FROM "react_gallery" ORDER BY "id" ASC;';
+    const queryString = `SELECT * FROM "react_gallery" ORDER BY "id" ASC;`;
     pool.query(queryString)
     .then((response) => {
         res.send(response.rows);
@@ -28,5 +28,21 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
     })
 })
+
+//POST Route
+router.post('/', (req, res) => {
+    const newItem = req.body;
+    console.log(newItem)
+    const queryString = `INSERT INTO "react_gallery" ("title", "path", "description")
+                         VALUES('${newItem.title}', '${newItem.path}', '${newItem.description}');`;
+    pool.query(queryString)
+    .then((response) => {
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })  
+});
+
 
 module.exports = router;
